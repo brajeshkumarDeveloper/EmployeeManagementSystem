a07bddd8766effc2f7ec56d8d6bfbfb1fa3b9ce7
brajeshkumarDeveloper <brajeshkewat8349@gmail.com>
2026-02-17 15:13:08 +0530

    corrected file name

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+# Configure the Terraform backend to use Amazon S3 for storing the state file and DynamoDB for state locking.
+terraform {
+  backend "s3" {
+    bucket = "employee-tf-s3-bucket" 
+    key    = "employee/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-lock-employee-table"
+  }
+}
